61919e4072ba027ea7c95593e6926b7970c587d1
julien <julien.milants@gmail.com>
2023-11-28 20:14:46 +0100

    initial commit

---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HomePages() {
+  return (
+    <>
+        <div className="main">
+            <div>
+                <h1>hello world</h1>
+            </div>
+        </div>
+    </>
+  )
+}
