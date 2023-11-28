22aca1666dc43d37eb39e04e369cb9e58b55dae2
julien <julien.milants@gmail.com>
2023-11-28 22:33:01 +0100

    start design

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -4,8 +4,13 @@ export default function HomePages() {
   return (
     <>
         <div className="main">
-            <div>
-                <h1>hello world</h1>
+            <div className='navigation'>
+                <div className="sidebar">
+                    <p>sidebar</p>
+                </div>
+                <div className="navbar">
+                    <p>navbar</p>
+                </div>
             </div>
         </div>
     </>
