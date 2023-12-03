923912ed7de29529ca0977d921c726b5a2b1fd66
julien <julien.milants@gmail.com>
2023-12-03 14:11:21 +0100

    format

--- src/pages/list/series.jsx
+++ src/pages/list/series.jsx
@@ -1,18 +1,23 @@
-import swat from "../../img/swat.jpeg"
-import prison from "../../img/prison.jpeg"
-import murder from "../../img/murder.jpeg"
-import one from "../../img/one.jpeg"
-import thecent from "../../img/the100.png"
-import f1 from "../../img/survive.jpeg"
-import lupin from "../../img/lupin.jpeg"
-import night from "../../img/night.jpeg"
-import peak from "../../img/peak.jpeg"
+import swat from "../../img/swat.jpeg";
+import prison from "../../img/prison.jpeg";
+import murder from "../../img/murder.jpeg";
+import one from "../../img/one.jpeg";
+import thecent from "../../img/the100.png";
+import f1 from "../../img/survive.jpeg";
+import lupin from "../../img/lupin.jpeg";
+import night from "../../img/night.jpeg";
+import peak from "../../img/peak.jpeg";
 export const series = [
   { name: "S.W.A.T", image: swat },
   { name: "prison break", image: prison },
   { name: "Murder", image: murder },
   { name: "One Piece", image: one },
-  { name: "The100", image: thecent },
+  {
+    name: "The100",
+    image: thecent,
+    description:
+      "The 100 se déroule dans un monde post-apocalyptique où 100 jeunes délinquants sont envoyés sur Terre pour déterminer si la planète est habitable. La série explore la survie, les dilemmes moraux, et les relations complexes entre les personnages.",
+  },
   { name: "Drive to survive", image: f1 },
   { name: "Lupin", image: lupin },
   { name: "The Night Agent", image: night },
